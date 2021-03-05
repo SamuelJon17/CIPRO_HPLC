a7271adb1b3f3fd848bf20c993bc1901ab90c402
Samueljon17 <samueljon@email.arizona.edu>
2021-03-05 11:15:42 -0500

    More than one page excel spreadsheets

--- Cipro_HPLC.py
+++ Cipro_HPLC.py
@@ -7,6 +7,7 @@ Created on Thu Mar  4 10:38:10 2021
 """
 import pandas as pd
 import os
+import math
 
 ## 
 ### import all csv files to the Data bin then run
@@ -14,24 +15,69 @@ import os
 data_path = os.getcwd() + '/Data'
 all_data_dict = {}
 all_data_list = []
-interested_retention = [15,19,20,22] ### change this to the retention times you are interestsed in
 counter = 0
+
+## Users change these 
+interested_retention = [15,19,20,22] ### change this to the retention times you are interestsed in
+excel_page_num = 46 ## Change the number of pages there are on a single excel file. If there are varying pages, run in batches
+                    ## by adding excel spreadsheets in the Data directory
+
 for j in os.listdir(data_path):
     if (j == '.DS_Store') or ('~$' in j):
         continue
     sheet_path = data_path + '/' + str(j)
-    series = pd.read_excel(sheet_path, sheet_name = 'Page 2', header = 6, index = False, index_col = None)
-    series = series.loc[:,~series.columns.str.match('Unnamed')]
-    series = series.dropna(thresh = 2)
-    identification = j.split('/')[-1].split('.')[0]
-    index = []
-    for i in range(len(series)):
-        if any(round(series['Peak\nRetention\nTime'][i],0) == interested_retention): 
-            index.append(i)
-    new_series = series.loc[index,:]
-    new_series['id'] = identification
-    all_data_dict[str(identification)] = new_series
-    all_data_list.append(new_series)
+    
+    # Header = None creates integer column names and grabs the whole sheet
+    #series = pd.read_excel(sheet_path, sheet_name = 'Page 2', header = None, index = False, index_col = None)
+    
+    for k in range(1, excel_page_num+1):
+        sheetname = 'Page ' + str(k)
+        series = pd.read_excel(sheet_path, sheet_name = sheetname, header = None, index_col = None)
+        #odd pages
+        if (k % 2) != 0:
+            identifcation = series.at[2,4]
+        # even pages
+        if (k % 2) == 0:
+            # Finds the first non-null in column 0, Generally this is where "Signal" is
+            start_index = series[0].first_valid_index() + 1
+            
+            #Remove all data above ""Peak Retention Time"
+            series = series.iloc[start_index:]
+            
+            # Renames column names to Peak retention time, etc.
+            headers = series.iloc[0]
+            series = pd.DataFrame(series.values[1:], columns = headers)
+            
+            # Removes all dead space from Excel merged columns
+            series = series.loc[:, series.columns.notnull()]
+            ## Old method
+            #series = series.loc[:,~series.columns.str.match('Unnamed')]
+           
+            # Removes all rows that have more than 2 NaN
+            series = series.dropna(thresh = 2)
+            index = []
+            for i in range(len(series)):
+                value = series['Peak\nRetention\nTime'][i]
+                if math.isnan(value):
+                    continue
+                if round(series['Peak\nRetention\nTime'][i],0) in interested_retention: 
+                    index.append(i)
+            new_series = series.loc[index,:]
+            new_series['id'] = identifcation
+            all_data_dict[str(identifcation)] = new_series
+            all_data_list.append(new_series)
+    
+    ## Old method
+    #identification = j.split('/')[-1].split('.')[0]
+    # index = []
+    # for i in range(len(series)):
+    #     if any(round(series['Peak\nRetention\nTime'][i],0) == interested_retention): 
+    #         index.append(i)
+    # new_series = series.loc[index,:]
+    # new_series['id'] = identification
+    # all_data_dict[str(identification)] = new_series
+    # all_data_list.append(new_series)
+
 all_data_list = pd.concat(all_data_list)
 
 file_name = 'data_0.csv'
