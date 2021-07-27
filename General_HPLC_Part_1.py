61dbd5a25984c243ebe86e9a654a12ba8abaae0d
Jon <samueljon@email.arizona.edu>
2021-07-27 14:56:11 -0400

    General HPLC clean and IFM modules

---
+++ General_HPLC_Part_1.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 10:38:10 2021
+
+@author: samueljon
+"""
+import pandas as pd
+import os
+from xlrd import XLRDError
+
+def hplc_clean(excel_page_num = 999, unit = None, reference_chem = None):
+    if (unit == None) | (unit == ''):
+        unit = input('Please input either thermo or agilent: ')
+    all_data_list = []
+    data_path = os.path.join(os.getcwd(), 'input', str(unit).lower())
+    for j in os.listdir(data_path):
+        if (j == '.DS_Store') or ('~$' in j):
+            continue
+        sheet_path = os.path.join(data_path, str(j))
+        if unit == 'agilent':
+            for k in range(1, excel_page_num+1):
+                sheetname = 'Page ' + str(k)
+                try:
+                    series = pd.read_excel(sheet_path, sheet_name = sheetname, header = None, index_col = None)
+                    # Identification Page [Generally odd]
+                    if type(series.at[2,0]) is str:
+                        identifcation = series.at[2,4]
+                        if 'blank' in identifcation.lower():
+                            identifcation = identifcation + ' ' + str(j) + ' ' + str(k)
+                        continue
+                    #elif len(series) >= 7:
+                    else: #Dealing with different Agilents [Beyonce]
+                        try:
+                            if type(series.at[6,1]) is str:
+                                identifcation = series.at[6,5]
+                                if 'blank' in identifcation.lower():
+                                    identifcation = identifcation + ' ' + str(j) + ' ' + str(k)
+                                continue
+                            elif 'signal' not in str(series.at[0,0]).lower():
+                                series = series.iloc[1:] # Remove the first row
+                                series = series.reset_index(drop=True) #Reset index numbers
+                        except KeyError:
+                            if 'signal' not in str(series.at[0,0]).lower():
+                                series = series.iloc[1:] # Remove the first row
+                                series = series.reset_index(drop=True) #Reset index numbers
+
+                    start_index = series[0].first_valid_index() + 1 # Finds the first non-null in column 0, Generally this is where "Signal" is
+                    series = series.iloc[start_index:] #Remove all data above ""Peak Retention Time"
+                    headers = series.iloc[0]
+                    series = pd.DataFrame(series.values[1:], columns = headers) # Renames column names to Peak retention time, etc.
+                    series = series.loc[:, series.columns.notnull()]  # Removes all dead space from Excel merged columns
+                    series = series.dropna(thresh = 2) # Removes all rows that have more than 2 NaN
+                    series = series[series['Peak\nRetention\nTime'].notna()]
+
+                    try:
+                        reference_series = series[series['Compound'].str.endswith('Cipro', na=False)]
+                        reference_value = reference_series.iloc[0]['Peak\nRetention\nTime']
+                        data['RRT (ISTD)'] = [float(x) / float(reference_value) for x in data['Peak\nRetention\nTime']]
+                    except:
+                        reference_value = input('Please input the RT (float) of reference sample to update the RRT or type no: ')
+                        if reference_value == 'no':
+                            reference_value = 1
+                        data['RRT (ISTD)'] = [float(x) / float(reference_value) for x in data['Peak\nRetention\nTime']]
+
+                    series['id'] = identifcation
+                    series['excel sheet'] = j
+                    series['page number'] = k
+                    all_data_list.append(series)
+
+                except XLRDError:
+                    print('Page {} does not exist in Excel spreadsheet {}'.format(k, j))
+                    break
+        else: #This is for Thermo units
+            identification = pd.read_excel(sheet_path, sheet_name = 'Integration', header = None, index_col = None).at[4,2]
+            data = pd.read_excel(sheet_path, sheet_name = 'Integration', header = 0, index_col = None, skiprows = 46).drop([0,1])
+            data = data[~data['No. '].isin(['n.a.', 'Total:'])]
+            data['id'] = identification
+            data['excel_sheet'] = pd.read_excel(sheet_path, sheet_name = 'Overview', header = None, index_col = None).at[3,2]
+            try:
+                if (reference_chem == None) | (reference_chem == ''):
+                    reference_chem = input('Please input the RT (float) of reference sample to update the RRT or type no: ')
+                    if reference_chem == 'no':
+                        reference_value = 1
+                else:
+                    reference_series = data[data['Peak Name'].str.endswith(reference_chem, na=False)]
+                    reference_value = reference_series.iloc[0]['RT']
+                data['RRT'] = [float(x) / float(reference_value) for x in data['RT']]
+            except:
+                data['RRT'] = data['RT']
+
+            try:
+                data = data.rename(columns={'Peak Name': 'Compound', 'RT': 'Peak\nRetention\nTime', 'RRT': 'RRT (ISTD)',
+                                    'LCAP ': 'Peak\nArea\nPercent', 'Amount ':'Compound Amount', 'Area ': 'Area', 'Height ': 'Height' })
+                data = data[['Peak\nRetention\nTime', 'RRT (ISTD)', 'Peak\nArea\nPercent', 'Area', 'Height', 'Compound',
+                             'Compound Amount', 'id', 'excel_sheet']]
+                #data = data.drop(columns=['As (EP)', 'No. '])
+
+            except KeyError: #Midaz does not have 'compound amount; and instead has 'S/N'
+                data = data.rename(columns={'Peak Name': 'Compound', 'RT': 'Peak\nRetention\nTime', 'RRT': 'RRT (ISTD)',
+                                            'LCAP ': 'Peak\nArea\nPercent', 'Area ': 'Area', 'Height ': 'Height'})
+                data = data[['Peak\nRetention\nTime', 'RRT (ISTD)', 'Peak\nArea\nPercent', 'Area', 'Height', 'Compound',
+                             'id', 'excel_sheet']]
+                #data = data.drop(columns=['As (EP)', 'No. ', 'S/N '])
+            all_data_list.append(data)
+    if len(all_data_list) >= 1:         
+        all_data_list = pd.concat(all_data_list)
+    else:
+        print('The directory, {}, is currently empty. Please make sure that files were properly added to the correct directory folder.'.format(unit))
+    return all_data_list
+    
+def save_data(data, unit = None):
+    if type(data) == list:
+        return None
+    else:
+        if (unit == None) | (unit == ''):
+            unit = input('Please input either thermo or agilent: ')
+        file_name = str(unit) + '-clean-data_0.csv'
+        csv_path = os.path.join(os.path.abspath("output"), 'clean', file_name)
+        if os.path.isfile(csv_path):
+            expand = 0
+            while True:
+                expand += 1
+                new_file_name = file_name.split("_")[0] + '_' + str(expand) + '.csv'
+                csv_path = os.path.join(os.path.abspath("output"), 'clean',new_file_name)
+                if os.path.isfile(csv_path):
+                    continue
+                else:
+                    file_name = csv_path
+                    break
+        data.to_csv(csv_path, index=False)
+    print('File was saved at {}'.format(csv_path))
+
+from timeit import default_timer as timer
+
+if __name__ == '__main__':
+      unit_ = input('Please enter either thermo or agilent: ')
+      start = timer()
+      dataframe = hplc_clean(unit = unit_)
+      save_data(dataframe, unit = unit_)
+      print('Total time to run code was {} sec'.format(timer() -start))
+      
+
+
---
+++ General_HPLC_Part_2.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 11 11:10:53 2021
+
+@author: samueljon
+"""
+import os
+import pandas as pd
+import ast
+import math
+
+def Import(file_name = None):
+    path = os.path.join(os.getcwd(), 'output', 'clean', file_name)
+    try:
+        series = pd.read_csv(path, header = 0)
+        return series
+    except:
+        return print('Please ensure there is only one clean file in IFM')
+
+def rrt_range(rrt, range_ = None, rounding_num = None):
+    rrt = float(rrt)
+    return [round(rrt-range_,rounding_num),round(rrt+range_,rounding_num)]
+
+def average(lst):
+    return sum(lst) / len(lst)
+
+def ifm(data = None, r = None, round_num = None):
+    data = data + '.csv'
+    series = Import(file_name = data)
+    lst = series['RRT (ISTD)'].tolist() #represents list of relative retention times, make sure to round prior
+    ranges = [rrt_range(lst[0], range_=r, rounding_num=round_num)] #represents ranges of unique list
+    range_dict = {str(rrt_range(lst[0], range_=r,rounding_num=round_num )):lst[0]}
+    new_lst = [] #represents unique list of relative retention times based on ranges, initialize with the first number
+    for i, num in enumerate(lst):
+        num = float(num)
+        if any(r[0] <= num <= r[1] for r in ranges): #if num is within any of the ranges, add the number that corresponds to that range to new_list
+            #boolean =  [r[0] <= num <= r[1] for r in ranges] #return the index where num is in ranges
+            index = [i for i, x in enumerate([r[0] <= num <= r[1] for r in ranges]) if x]
+            if len(index) == 2:
+                if abs(num - average(ranges[index[0]])) > (abs(num - average(ranges[index[1]]))):
+                    index = [index[1]]
+                else:
+                    index = [index[0]]
+            new_lst.append(range_dict[str(ranges[index[0]])])
+        else:
+            ranges.append(rrt_range(num,range_=r,rounding_num=round_num))
+            range_dict.update({str(rrt_range(num, range_=r,rounding_num=round_num)):num})
+            new_lst.append(num)
+    series['RRT (ISTD)_new'] = new_lst
+    new_set = set(new_lst) #unique values of list + sort
+    final_dict = {}
+    subset = series.groupby(['id']).apply(lambda x: x[['RRT (ISTD)_new', 'Peak\nArea\nPercent', 'Compound']].values).to_dict()
+    for key, value in subset.items():
+            test_list = [i if i in subset[key][:,0].tolist() else 0 for i in list(new_set)]
+            for idx2, j in enumerate(subset[key][:,0].tolist()):
+                if subset[key][:,0][idx2] in test_list:
+                    test_list[test_list.index(j)]=subset[key][:,1].tolist()[idx2]
+            final_dict.update({key: test_list})
+    final_rrt_lst = [round(x,round_num) for x in new_set]
+    df = pd.DataFrame.from_dict(final_dict, orient='index', columns = final_rrt_lst)
+
+    try:
+        df = df.reindex(sorted(df.columns), axis = 1)
+        df['Total LCAP'] = df.sum(axis = 1)
+        
+        ''' Checking if the total LCAP of each sample is the same as the raw data.
+            If they are all the same, the output is reliable.
+            If any of them do not match, it most likely due to the grouping error, where two peaks from
+            the same sample are too close to each other and were mistakenly group as one peak.
+            In that case, user can use a smaller range, bigger rounding number, or both.
+        '''
+        check = []
+        for k in subset.keys():
+            subset[k] = pd.DataFrame(subset[k])
+            subset[k] = subset[k].append(subset[k].sum(numeric_only=True), ignore_index=True) # Adds a sum row to each sample
+            if round(df.loc[k,'Total LCAP'], 2) != round(subset[k].loc[len(subset[k])-1,1], 2):
+                check.append(k)
+        if len(check) >= 1:
+            # CGREEN = '\033[1;32;40m'
+            # CEND = '\033[1;30;46m'
+            print(('\033[31m'+ 'Total LCAP of samples {} do NOT match with raw data. Please try smaller range, bigger rounding, or both' + '\033[30m').format(check))
+        return df
+    except ValueError:
+        print('Error at rounding {} and range {}'.format(round_num,r))
+        return final_rrt_lst
+
+def save_data(data):
+    if type(data) == list:
+        return None
+    else:
+        file_name = 'IFM-summary-_0.csv'
+        csv_path = os.path.join(os.getcwd(), 'output', 'ifm', file_name)
+        if os.path.isfile(csv_path):
+            expand = 0
+            while True:
+                expand += 1
+                new_file_name = file_name.split("_")[0] + '_' + str(expand) + '.csv'
+                csv_path = os.path.join(os.getcwd(), 'output', 'ifm', new_file_name)
+                if os.path.isfile(csv_path):
+                    continue
+                else:
+                    file_name = csv_path
+                    break
+        data.to_csv(csv_path, index=True)
+    print('File was saved at {}'.format(csv_path))
+
+from timeit import default_timer as timer
+if __name__ == '__main__':
+    clean_data = input('Please input clean file name: ')
+    if len(clean_data) == 0:
+        clena_data = 'thermo-clean-data_8'
+    start = timer()
+    df = ifm(data = clean_data, r = 0.01, round_num = 4)
+    save_data(data = df)     
+    print('Total time to run code was {} sec'.format(timer() -start))  
+    
+        
+        
